ed184ff72e5259303918d82ef72eb574e1d7bd46
Muhammadzakirha <rzaki9353@gmail.com>
2021-01-05 14:46:53 +0000

    Why

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
-
 #variabels
 UserInput = ""
 ChatBot_Name = "zaki"
 ChatBot_Name_Error = "Error"
 UserInput = str.lower(input("guest: "))
+import pyfiglet
+textascii = pyfiglet.figlet_format(":\nsome text")
+textlist = [
+  "ascii"
+  "random"
+  "name"
+]
 
 #Pesan
-Pesan = ["hi", "your owner", "", "say"]
-respon = [": Hello guest.", ": you", ": whats?", ": not support"]
+Pesan = ["hi", "your owner", "", "say", "ascii"]
+respon = [": Hello guest.", ": you", ": whats?", ": not support", textascii]
 
 #respon
 if UserInput in Pesan:
