ff5cf49a9b6326b55fd4d14ad07a91045e96f39c
Muhammadzakirha <rzaki9353@gmail.com>
2021-01-06 03:44:10 +0000

    For me busnimens

--- main.py
+++ main.py
@@ -1,24 +1,27 @@
 #variabels
 UserInput = ""
 ChatBot_Name = "zaki"
-ChatBot_Name_Error = "Error"
+ChatBot_Name_Error = "console"
 UserInput = str.lower(input("guest: "))
 import pyfiglet
-textascii = pyfiglet.figlet_format(":\nsome text")
-textlist = [
-  "ascii"
-  "random"
-  "name"
-]
+textascii = pyfiglet.figlet_format(":\ntest")
 
 #Pesan
-Pesan = ["hi", "your owner", "", "say", "ascii"]
-respon = [": Hello guest.", ": you", ": whats?", ": not support", textascii]
+Pesan = ["hi", "your owner", "say", "ascii"]
+respon = [": Hello guest.", ": you", ": not support", textascii]
 
 #respon
 if UserInput in Pesan:
 	index = Pesan.index(UserInput)
 	print(ChatBot_Name + respon[index])
 
-if UserInput not in Pesan:
-	print(ChatBot_Name_Error + ': not found')
+if UserInput == "asciicus":
+  asciicust = input(ChatBot_Name +": whats your text: ")
+  asciicusto = pyfiglet.figlet_format(asciicust)
+  print(ChatBot_Name + ":\n",asciicusto)
+
+if UserInput == 'command':
+  print(ChatBot_Name + ": ascii\nasciicus")
+
+if UserInput == "":
+  print(ChatBot_Name_Error + ": an error not found")
