66002952475426d7cf43b7b76a57829842580ae5
Muhammadzakirha <rzaki9353@gmail.com>
2021-01-07 03:52:54 +0000

    Hah

--- main.py
+++ main.py
@@ -1,27 +1,23 @@
-#variabels
-UserInput = ""
-ChatBot_Name = "zaki"
-ChatBot_Name_Error = "console"
-UserInput = str.lower(input("guest: "))
-import pyfiglet
-textascii = pyfiglet.figlet_format(":\ntest")
+while (True):
+	#var
+	UserInput = ""
+	ChatBot_Name = "zaki"
+	ChatBot_Name_Error = "console"
+	UserInput = str.lower(input("guest: "))
+	import pyfiglet
+	textascii = pyfiglet.figlet_format("\ntest")
 
-#Pesan
-Pesan = ["hi", "your owner", "say", "ascii"]
-respon = [": Hello guest.", ": you", ": not support", textascii]
-
-#respon
-if UserInput in Pesan:
-	index = Pesan.index(UserInput)
-	print(ChatBot_Name + respon[index])
-
-if UserInput == "asciicus":
-  asciicust = input(ChatBot_Name +": whats your text: ")
-  asciicusto = pyfiglet.figlet_format(asciicust)
-  print(ChatBot_Name + ":\n",asciicusto)
-
-if UserInput == 'command':
-  print(ChatBot_Name + ": ascii\nasciicus")
-
-if UserInput == "":
-  print(ChatBot_Name_Error + ": an error not found")
+	if UserInput == "hi":
+		print(ChatBot_Name + ": hello")
+	elif UserInput == "your owner":
+		print(
+		    ChatBot_Name +
+		    ": if you fork this project is your projects but official owner is muhamadzakirha"
+		)
+	elif UserInput == "say":
+		print(ChatBot_Name + ": ", UserInput)
+	elif UserInput == "ascii":
+		print(ChatBot_Name + ":\n", textascii)
+	elif UserInput == "quit":
+		print(ChatBot_Name + ": bye thanks use me")
+		break
