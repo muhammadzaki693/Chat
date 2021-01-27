ab29691fb3bb2273ce31aa0b569169325a2eaea2
Muhammadzakirha <rzaki9353@gmail.com>
2021-01-27 09:37:15 +0000

    What did you change?

--- main.py
+++ main.py
@@ -1,25 +1,34 @@
-while (True):
-	#var
-	UserInput = ""
-	ChatBot_Name = "zaki"
-	ChatBot_Name_Error = "console"
-	UserInput = str.lower(input("guest: "))
-	import pyfiglet
-	textascii = pyfiglet.figlet_format("\ntest")
+def chat():
+  while (True):
+    
+	  #var
+	  UserInput = ""
+	  ChatBot_Name = "zaki"
+	  ChatBot_Name_Error = "console"
+	  UserInput = str.lower(input("guest: "))
+	  import pyfiglet
+	  textascii = pyfiglet.figlet_format("test")
+	  
+	  #say quit
+	  if UserInput == "quit" or UserInput == "end":
+		  print(ChatBot_Name + ": bye thanks use me")
+		  break
 	
-	#say quit
-	if UserInput == "quit" or UserInput == "end":
-		print(ChatBot_Name + ": bye thanks use me")
-		break
 	
-	if UserInput == "hi" or UserInput == "hello":
-		print(ChatBot_Name + ": hello")
-	elif UserInput == "your owner" or UserInput == "owner":
-		print(
+	  if UserInput == "hi" or UserInput == "hello":
+		  print(ChatBot_Name + ": hello")
+	  elif UserInput == "asciicustom" or UserInput == "asciicus":
+	    cusascii = input("your text: ")
+	    cusascii2 = pyfiglet.figlet_format(cusascii)
+	    print(ChatBot_Name + "\n" + cusascii2)
+	  elif UserInput == "your owner" or UserInput == "owner":
+		  print(
 		    ChatBot_Name +
 		    ": if you fork this project is your projects but official owner is muhamadzakirha"
 		)
-	elif UserInput == "ascii" or UserInput == "acii":
-		print(ChatBot_Name + ":\n", textascii)
-	else:
-	  print(ChatBot_Name + ": whats sorry im not understand")
+	  elif UserInput == "ascii" or UserInput == "acii":
+		  print(ChatBot_Name + ":\n", textascii)
+	  else:
+	    print(ChatBot_Name + ": whats sorry im not understand")
+
+chat()
